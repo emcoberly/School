f916d62993aaa85b56e5bf391058de7ace89aaeb
Doug Thomsen <doug.thomsen@wallawalla.edu>
2021-01-05 16:16:05 -0800

    Signed-off-by: Doug Thomsen <doug.thomsen@wallawalla.edu>

---
+++ homework/hw01/starTrek.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+ *
+ * hw01: A mad-libs introduction to StarTrek
+ *
+ * File Name: starTrek.cpp
+ * Name:      ?
+ * Course:    CPTR 141
+ *
+ */
+
+#include <iostream>  // included for cin and cout processing
+using namespace std;
+
+int main() {
+    // define your variables below
+
+    // gather user input using cin << variableName
+
+    // print out the intro
+
+    return 0;
+}
