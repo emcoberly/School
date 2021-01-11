c16628cd4085eeb60f0b38aa1887c94334231618
cobeed <cobeed@cs.wallawalla.edu>
2021-01-11 16:08:38 +0000

    Commit homework/hw00

--- homework/hw00/quote.cpp
+++ homework/hw00/quote.cpp
@@ -12,15 +12,15 @@
 using namespace std;
 
 int main() {
-   
-    // insert your code here
-    cout << "The competent programmer is fully aware" << endl;
-    cout << "  of the strictly limited size of his own skull;" << endl;
-    cout << "therefore he approaches the programming task" << endl;
-    cout << "  in full humility," << endl;
-    cout << "and among other things" << endl;
-    cout << "  he avoids clever tricks like the plague." << endl << endl;
-    cout << "- E. Dijkstra (1972)" << endl;
 
-    return 0;
+  // insert your code here
+  cout << "The competent programmer is fully aware" << endl;
+  cout << "  of the strictly limited size of his own skull;" << endl;
+  cout << "therefore he approaches the programming task" << endl;
+  cout << "  in full humility," << endl;
+  cout << "and among other things" << endl;
+  cout << "  he avoids clever tricks like the plague." << endl << endl;
+  cout << "- E. Dijkstra (1972)" << endl;
+
+  return 0;
 }
