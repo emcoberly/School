f450f3c2f3ab6c1c51546464cb7e0334da4eebbf
cobeed <cobeed@cs.wallawalla.edu>
2021-02-01 08:59:30 +0000

    Commit homework/hw06

--- homework/hw06/pwordChecker.cpp
+++ homework/hw06/pwordChecker.cpp
@@ -1,9 +1,9 @@
 /*************************************************************************
  *
  * hw06: Determine if a password meets the required specifications.
- * 
+ *
  * File Name: pwordChecker.cpp
- * Name:      ???
+ * Name:      Eddie Coberly
  * Course:    CPTR 141
  *
  */
@@ -13,29 +13,63 @@
 using namespace std;
 
 int main() {
-   
-   // define variables
-   
-   // prompt for input
-   cout << "Please enter a password with the following characteristics:" << endl;
-   cout << "  - at least 10 characters long" << endl;
-   cout << "  - contains at least one digit" << endl;
-   cout << "  - contains at least one lower-case letter" << endl;
-   cout << "  - contains at least one upper-case letter" << endl;
-   cout << "  - contains at least one non-alphanumeric character" << endl;
-   cout << "  - does not contain any consecutive duplicate characters (e.g. 'aa')" << endl << endl;
 
-   
-   // collect input
+  // define variables
+  string userPass;
+  char passCheck, passCheck2;
+  int passIndex = 0, passIndex2 = 1;
+  bool belowTen = true, missingDigit = true, missingLower = true,
+       missingUpper = true, missingChar = true, isConsecutive = false;
 
-   
-   // loop to check password characters
-   
-   
-   // display result
+  // prompt for input
+  cout << "Please enter a password with the following characteristics:" << endl;
+  cout << "  - at least 10 characters long" << endl;
+  cout << "  - contains at least one digit" << endl;
+  cout << "  - contains at least one lower-case letter" << endl;
+  cout << "  - contains at least one upper-case letter" << endl;
+  cout << "  - contains at least one non-alphanumeric character" << endl;
+  cout
+      << "  - does not contain any consecutive duplicate characters (e.g. 'aa')"
+      << endl
+      << endl;
 
-   
-   // end the program
-   return 0;
-   
+  // collect input
+  cout << "Password: ";
+  cin >> userPass;
+
+  // loop to check password characters
+  do {
+    passCheck = userPass.at(passIndex);
+    if (passCheck==passCheck2) {
+        isConsecutive = true;
+    }
+    if (userPass.length() >= 10) {
+      belowTen = false;
+    }
+    if (isdigit(passCheck)) {
+      missingDigit = false;
+    }
+    if (islower(passCheck)) {
+      missingLower = false;
+    }
+    if (isupper(passCheck)) {
+      missingUpper = false;
+    }
+    if (!isalnum(passCheck)) {
+      missingChar = false;
+    }
+    passCheck2 = passCheck;
+    passIndex += 1;
+  } while (passIndex < userPass.length());
+
+  // display result
+  if(!(belowTen || missingDigit || missingLower || missingUpper || missingChar || isConsecutive)) {
+      cout << "Your password has been accepted." << endl;
+  } else {
+      cout << "Your password does not meet the above criteria." << endl;
+  }
+
+  // end the program
+
+  return 0;
 }
