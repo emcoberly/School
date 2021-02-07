16315c9799cb60883e3ff192b0479d092090eeb0
Doug Thomsen <doug.thomsen@wallawalla.edu>
2021-02-07 14:29:55 -0800

    Signed-off-by: Doug Thomsen <doug.thomsen@wallawalla.edu>

---
+++ in-class/0208/activity10b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+  // put first 100 powers of two in x
+  vector<double> x(100);
+  for (int i = 0; i < x.size(); i++) {
+    x.at(i) = pow(2,i);
+  }
+
+  // define scores
+  vector<int> y = {95, 68, 82, 75, 84, 79, 73, 91};
+
+  // sum the elements of scores
+  int sum = 0;
+  for (int i = 0; i < y.size(); i++) {
+      sum += y.at(i);
+  }
+
+  return 0;
+}
