3192f29e2ead7a75899cd5fce82339e4588fcaa7
cobeed <cobeed@cs.wallawalla.edu>
2021-03-09 07:57:52 +0000

    Commit homework/hw15

--- homework/hw15/tttFunctions.cpp
+++ homework/hw15/tttFunctions.cpp
@@ -12,35 +12,35 @@
 using namespace std;
 
 char getWinner(char grid[][NUM_ROWS], int theSize) {
-    char winnerChar = ' ';
-    for (int i = 0; i < theSize; i++) {
-        if (grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) {
-            if (grid[i][0] == 'X') {
-                winnerChar = 'X';
-            } else if (grid[i][0] == 'O') {
-                winnerChar = 'O';
-            }
-        } else if (grid[0][i] == grid [1][i] && grid[1][i] == grid[2][i]) {
-            if (grid[0][i] == 'X') {
-                winnerChar =  'X';
-            } else if (grid[0][i] == 'O') {
-                winnerChar =  'O';
-            }
-        } else if (grid[0][0] == grid [1][1] && grid[1][1] == grid[2][2]) {
-            if (grid[0][0] == 'X') {
-                winnerChar =  'X';
-            } else if (grid[0][0] == 'O') {
-                winnerChar =  'O';
-            }
-        } else if (grid[0][2] == grid [1][1] && grid[1][1] == grid[2][0]) {
-            if (grid[0][2] == 'X') {
-                winnerChar =  'X';
-            } else if (grid[0][2] == 'O') {
-                winnerChar =  'O';
-            }
-        } else {
-            winnerChar =  '-';
-        }
+  char winnerChar = ' ';
+  for (int i = 0; i < theSize; i++) {
+    if (grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) {
+      if (grid[i][0] == 'X') {
+        winnerChar = 'X';
+      } else if (grid[i][0] == 'O') {
+        winnerChar = 'O';
+      }
+    } else if (grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) {
+      if (grid[0][i] == 'X') {
+        winnerChar = 'X';
+      } else if (grid[0][i] == 'O') {
+        winnerChar = 'O';
+      }
+    } else if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) {
+      if (grid[0][0] == 'X') {
+        winnerChar = 'X';
+      } else if (grid[0][0] == 'O') {
+        winnerChar = 'O';
+      }
+    } else if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]) {
+      if (grid[0][2] == 'X') {
+        winnerChar = 'X';
+      } else if (grid[0][2] == 'O') {
+        winnerChar = 'O';
+      }
+    } else {
+      winnerChar = '-';
     }
-    return winnerChar;
+  }
+  return winnerChar;
 }
