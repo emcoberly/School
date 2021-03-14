27f48b9cac707aaa07404d972e35361ffcc1b511
cobeed <cobeed@cs.wallawalla.edu>
2021-03-14 08:22:16 +0000

    Commit homework/hw17

--- homework/hw17/maxFunctions.cpp
+++ homework/hw17/maxFunctions.cpp
@@ -9,24 +9,51 @@
 #include "maxFunctions.h"
 #include <iostream>
 #include <sstream>
+#include <vector>
 using namespace std;
 
 int findMax(string userString) {
   istringstream sin(userString);
-  string inputArray[1000];
+  vector<int> inputArray;
+  int userNum;
   int max = 0;
-  int i = 0;
-  while (getline(sin, userString)) {
-    inputArray[i] = userString;
-    i++;
+  int count = 0;
+  while (sin >> userString) {
+    bool trueInt = true;
+    if (userString.at(userString.size() - 1) == '.') {
+      userString.erase(userString.begin() + userString.size() - 1); // Looked up erase() function from http://www.cplusplus.com/reference/string/string/erase/
+    }
+    for (int i = 0; i < userString.size(); i++) {
+      if (!isdigit(userString.at(i))) {
+        trueInt = false;
+        break;
+      }
+    }
+    if (trueInt) {
+      userNum = stoi(userString); // stoi() came from https://www.geeksforgeeks.org/converting-strings-numbers-cc/
+      inputArray.push_back(userNum);
+    }
+    count++;
   }
-  for (int j = 0; j < i; j++) {
-    cout << inputArray[j] << ",";
+  for (int i = 0; i < inputArray.size() - 1; i++) {
+    if (inputArray.at(i + 1) > inputArray.at(i)) {
+      inputArray.push_back(inputArray.at(i + 1));
+    }
   }
+  max = inputArray.at(inputArray.size() - 1);
   return max;
 }
 
 string getSet(const int array[], int size) {
-  string userString;
-  return userString;
+  ostringstream sout;
+  sout << "{";
+  if (size > 0) {
+    for (int i = 0; i < size - 1; i++) {
+        sout << " " << array[i] << ",";
+    }
+    sout << " " << array[size - 1];
+  }
+  sout << " }" << endl;
+
+  return sout.str();
 }
